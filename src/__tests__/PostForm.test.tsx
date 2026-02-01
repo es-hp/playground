990644dc3844a5a7635a86f33e7b6ffa9e7790a8
Helen <204595204+es-hp@users.noreply.github.com>
2026-01-31 22:41:41 -0500

    Fixed typescript error for vitest mock axios

--- src/__tests__/PostForm.test.tsx
+++ src/__tests__/PostForm.test.tsx
@@ -2,24 +2,22 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+import type { AxiosStatic } from "axios";
 
 // Vitest testing utilities
-import { vi, describe, test, expect } from "vitest";
+import { vi, describe, test, expect, type Mocked } from "vitest";
 
 // Third-Party libraries
 import axios from "axios";
 
 // Local imports
 import { PostForm } from "../components/PostForm";
-import type { PostData, PostResponse } from "../components/PostForm";
 
 // Replace axios with a mock
 vi.mock("axios");
 
 // Manually write the type for the mock axios response for TS
-const mockedAxios = axios as unknown as {
-  post: vi.Mock<Promise<{ data: PostResponse }>, [string, PostData]>;
-};
+const mockedAxios = axios as Mocked<AxiosStatic>;
 
 describe("PostForm Component Integration Test", () => {
   test("submits the form data correctly", async () => {
